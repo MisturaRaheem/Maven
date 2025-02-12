65e2c028c0d228d4b288437471d7ed8f834041eb
Mistura Raheem <mraheem5@my.centennialcollege.ca>
2025-02-12 15:00:54 -0500

    Initial commit

---
+++ src/main/java/org/mr/mavenwebapp/App.java
@@ -0,0 +1,13 @@
+package org.mr.mavenwebapp;
+
+/**
+ * COMP367
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to COMP367" );
+    }
+}
